2b9828e00b845177f125a3fa956e1c781912f8ae
Alexandr Larionov <alarionov93@yandex.ru>
2021-03-24 01:54:03 +0500

    latest version here

--- push_latest.sh
+++ push_latest.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
+
+ps h -p $$ # show info about proccess for current PID
 base_v=v0.0.
 # commit amd push to git
 git add *
-echo -n "Enter commit msg and press [ENTER]: "
+echo "Enter commit msg and press [ENTER]: "
 read msg
 git commit -a -m "$msg"
-echo "..and fix some \xf0\x9f\x92\xa9"
+# echo "..and fix some \xf0\x9f\x92\xa9"
 git push
 ver_num=$(cat version.txt | tail -1)
-git tag "$base_v"$((ver_num+1))
-git push --tags
-echo $((ver_num+1))\r\n >> version.txt
-echo '\xf0\x9f\xa4\x98'
-echo '\xf0\x9f\x8d\xba'
+# git tag "$base_v"$((ver_num+1))
+# git push --tags
+echo $((ver_num+1)) >> version.txt
+echo "That's \xf0\x9f\xa4\x98!"
+echo "Let's drunk some vodka with \xf0\x9f\x8d\xba!"
--- test.sh
+++ test.sh
@@ -4,3 +4,6 @@ echo $((a+1))
 
 # printf \\U0001f4a9\\r\\n | hexdump
 echo '\xf0\x9f\x92\xa9'
+
+echo '\xf0\x9f\xa4\x98'
+echo '\xf0\x9f\x8d\xba'
