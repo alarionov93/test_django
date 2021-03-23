a3d6aece85249ecd92b7cbc4954bd8bdae93f100
Alexandr Larionov <alarionov93@yandex.ru>
2021-03-24 01:11:09 +0500

    all done with push script ..and fix some \U0001f4a9

--- push_latest.sh
+++ push_latest.sh
@@ -5,9 +5,11 @@ base_v=v0.0.
 git add *
 echo -n "Enter commit msg and press [ENTER]: "
 read msg
-git commit -a -m "$msg"
+git commit -a -m "$msg ..and fix some \U0001f4a9"
+echo "..and fix some \U0001f4a9"
 git push
 ver_num=$(cat version.txt | tail -1)
-echo git tag "$base_v"$((ver_num))
-echo $((ver_num)) >> version.txt
-
+git tag "$base_v"$((ver_num+1))
+echo $((ver_num+1))\r\n >> version.txt
+echo '\U0001f918'
+echo '\U0001f37a'
